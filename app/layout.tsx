143e7638b834c9391c6e61d5f49f92accb572cad
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-02-04 10:05:56 +0530

    dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/general/Navbar";
+import { ThemeProvider } from "@/components/general/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,11 +29,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeProvider defaultTheme="dark" attribute="class" enableSystem disableTransitionOnChange >
         <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
          <Navbar /> 
          {children}
 
         </div>
+        </ThemeProvider>
       </body>
     </html>
   );
