3276c58768429f94f342f7e1e34db0c6feaa9f0f
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-02-05 14:29:15 +0530

    onboarding page

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,13 @@
+import OnBoardingForm from '@/components/forms/onboarding/OnBoardingForm'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen w-screen flex flex-col items-center justify-center py-10 '>
+        <OnBoardingForm />
+
+    </div>
+  )
+}
+
+export default page
---
+++ components/forms/onboarding/OnBoardingForm.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import Image from 'next/image'
+import React, { useState } from 'react'
+import Logo from '@/public/logo.png'
+import { Card, CardContent } from '@/components/ui/card'
+
+type UserSelectionType='company' | 'jobSeeker' | null;
+
+const OnBoardingForm = () => {
+
+    const [step,setStep]=useState(1);
+    const [userType,setUserType]=useState<UserSelectionType>(null);
+
+    function handleUserTypeSelection(type:UserSelectionType){
+        setUserType(type);
+        setStep(2);
+    }
+
+
+    function renderStep(){
+        switch(step){
+            case 1:
+                return <p>User type selection form</p>
+            case 2:
+                return userType==="company" ?(
+                    <p>user is company</p>
+                ):(
+                  <p>user is job seeker</p>
+                )
+
+            default:
+                return null
+        }
+    }
+
+
+  return (
+    <>
+    <div className='flex items-center gap-4 mb-10'>
+        <Image src={Logo} alt='Logo' width={50} height={50} />
+        <h1 className='text-4xl font-bold'>Job <span className='text-primary'>Board</span></h1>
+
+    </div>
+
+    <Card className='max-w-lg w-full  '>
+        <CardContent className='p-6'>
+            {renderStep()}
+
+        </CardContent>
+    </Card>
+    </>
+  )
+}
+
+export default OnBoardingForm
