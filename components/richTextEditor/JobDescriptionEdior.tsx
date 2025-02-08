8ab88ee2368c2e4c10cb6aaeee87d9098db3bf84
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-02-08 13:19:09 +0530

    text editor

--- components/richTextEditor/JobDescriptionEdior.tsx
+++ components/richTextEditor/JobDescriptionEdior.tsx
@@ -2,13 +2,24 @@ import React from 'react'
 import {EditorContent, useEditor} from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import { MenuBar } from './MenuBar';
+import TextAlign from "@tiptap/extension-text-align"
+import Typography from '@tiptap/extension-typography'
 
 const JobDescriptionEdior = () => {
     const editor=useEditor({
         extensions:[
-            StarterKit
+            StarterKit,
+            TextAlign.configure({
+                types:['heading','paragraph']
+            }),
+            Typography
         ],
         immediatelyRender:false,
+        editorProps:{
+            attributes:{
+                class:'min-h-[300px] p-4 max-w-none focus:outline-none prose prose-sm sm:prose lg:prose-lg xl:prose-xl dark:prose-invert'
+            }
+        }
 
     });
 
